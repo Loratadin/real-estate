2764f1df2db40da5d2a9b3ac9526f2098a87a10a
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-02 16:17:50 -0500

    Clean up the code

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 export default (
 <BrowserRouter>
 <div>
-<Route path="/" component={App} ></Route>
-<Route exact path="/" component={Home} ></Route>
+<Route path="/" component={App} />
+<Route path="/" component={Home} />
 </div>
 </BrowserRouter>
 );
