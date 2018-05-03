821d58ca674f0286af25294cc9610a3ebe8b434a
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-03 06:12:00 -0500

    Set up the styles for container

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Container from '../theme/grid/Container';
 
 const Image = styled.img`
 width: 100%;
@@ -10,12 +11,12 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>
+      <Container>
         <Image src={require('../assets/us.jpg')} alt="us"/>
         <p>
         Aliquam vitae sem eu sem pretium iaculis. Duis malesuada consectetur velit ut pretium. Nullam posuere est eu nunc tempor aliquam. Nam dui quam, tempor eu dui sit amet, luctus elementum odio. Mauris vel venenatis metus. Etiam venenatis ullamcorper nibh, porta rutrum urna molestie a. Vivamus scelerisque eros quis blandit dapibus. Etiam blandit mi quis erat lobortis, sit amet ultricies eros condimentum.
         </p>
-      </div>
+      </Container>
     );
   }
 }
---
+++ src/theme/grid/Container.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export default styled.div`
+padding: 30px 120px 0 120px;
+`;
